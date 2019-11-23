41c082b75de3fe223e52b7d76999e1a570373a58
Yajat Vishwakk <yajat472001@gmail.com>
2019-11-23 14:23:07 +0530

    Hello world - First Commit

---
+++ reactnative0/App.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <Text style= {{padding:50}}> Hello World!</Text>
+  );
+};
+
+export default App;
